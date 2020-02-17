29471d7a115ec6f9d67a93571c579ced8d1e4f7f
Pavel <dniwedna@yandex.ru>
2020-02-18 01:09:14 +0300

    fill_words_tabbles
    asd

--- create_base.py
+++ create_base.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
  
 conn = sqlite3.connect("project.db")
 
@@ -18,4 +19,29 @@ def create_tables(con):
         )
     con.commit()
 
-create_tables(conn)
+
+def fill_tables(con):
+    with open('english_words.json', 'r', encoding='utf-8') as english_words:
+        words = json.load(english_words)
+        
+        cursor = con.cursor()
+        
+        i = 0
+        for elem in words:           
+            i+= 1
+            cursor.execute('''INSERT INTO words(word, translation)\
+                                 VALUES(?, ?)''', (elem['word'], elem['translation']) )
+            
+            for ex in elem['examples']:
+                cursor.execute('''INSERT INTO examples(sentence, word_id)\
+                                 VALUES(?, ?)''', (ex, i) )
+        
+        con.commit()
+
+
+#create_tables(conn)
+fill_tables(conn)
+cursor = conn.cursor()
+cursor.execute("SELECT word, translation FROM words")
+words = cursor.fetchall()
+print(words)
